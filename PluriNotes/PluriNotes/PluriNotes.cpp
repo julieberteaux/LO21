b02153bc2652aaf23a6b15f9e125130f6058fd72
Julie Berteaux <julie.berteaux96@gmail.com>
2017-04-07 15:56:17 +0200

    initialisation du repo et créations des fichiers cpp et h

---
+++ PluriNotes/PluriNotes/PluriNotes.cpp
@@ -0,0 +1,10 @@
+//
+//  PluriNotes.cpp
+//  PluriNotes
+//
+//  Created by Julie Berteaux on 07/04/2017.
+//  Copyright © 2017 Julie Berteaux. All rights reserved.
+//
+
+#include <stdio.h>
+#include "PluriNotes.h"
---
+++ PluriNotes/PluriNotes/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  PluriNotes
+//
+//  Created by Julie Berteaux on 07/04/2017.
+//  Copyright © 2017 Julie Berteaux. All rights reserved.
+//
+
+#include <iostream>
+#include "PluriNotes.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

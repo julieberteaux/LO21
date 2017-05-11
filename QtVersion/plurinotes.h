744cd7fb5964019514122949e5b6876026d2cb4f
Laura <laura.liechitz@gmail.com>
2017-05-11 13:35:43 +0200

    update class Date

--- QtVersion/plurinotes.h
+++ QtVersion/plurinotes.h
@@ -14,31 +14,31 @@ public:
     const QString & getInfo() const{return info;}
 };
 
-class Date{
-    int day;
-    int month;
-    int year;
-public:
-    int getDay(){return day;}
-    int getMonth(){return month;}
-    int getYear(){return year;}
-    void setDay(const int & d){
-        if ((d>=1) && (d<=31))
-            day=d;
-        else throw Exception("Jour incorrect");
-    }
-    void setMonth(const int & m){
-        if ((m>=1) && (m<=12))
-            month=m;
-        else throw Exception("Mois incorrect");
-    }
-    void setYear(const int & y){year=y;}
-    Date(int d, int m, int y){
-        setDay(d);
-        setMonth(m);
-        setYear(y);
-    }
-};
+class Date {
+    public:
+        //! Constructeur ‡ partir d'un jour, mois, annÈe
+        /*! \param j jour avec 1<=j<=31
+            \param m mois avec 1<=m<=12
+            \param a annÈe avec a>=0
+            */
+        Date(unsigned int short j=1, unsigned int short m=1, unsigned int a=0):jour(1),mois(1),annee(0){ setDate(j,m,a); }
+        // mÈthodes
+        unsigned short int  getJour() const { return jour; } //<! Retourne le jour de la date
+        unsigned short int  getMois() const { return mois; } //<! Retourne le mois de la date
+        unsigned int getAnnee() const { return annee; } //<! Retourne l'annÈe de la date
+        void setDate(unsigned short int j, unsigned short int m, unsigned int a); //!< initialisation de la date
+        void afficher(std::ostream& f=std::cout) const; //!< affiche le date sous le format JJ/MM/AAAA
+        bool operator==(const Date& d) const; //<! d1==d2 retourne vrai si les deux dates sont Ègales
+        bool operator<(const Date& d) const; //<! Compare deux dates dans le temps : d1<d2 retourne true si d1 est avant d2
+        int operator-(const Date& d) const; //<! Retourne le nombre de jours sÈparant les deux dates
+        Date demain() const; //<! Retourne la date du lendemain
+        Date operator+(unsigned int nb) const; //<!Retourne la date de dans nb jours
+    private:
+        // attributs
+        unsigned short int jour; // jour entre 1 et 31
+        unsigned short int mois; // mois entre 1 et 12
+        unsigned int annee;
+    };
 
 /********************************** Note & co ****************************/
 

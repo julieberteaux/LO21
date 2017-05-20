506d5098600f67dfb309ea5abcae6dd5e11d1583
Laura <laura.liechitz@gmail.com>
2017-05-20 17:15:25 +0200

    add reference relation in main+fix some bugs

--- QtVersion/main.cpp
+++ QtVersion/main.cpp
@@ -12,15 +12,18 @@ int main() {
         Note& n0=manager.getNote(0);
         Note& n2=manager.getNote(1);
         //Note& n3=manager.getNote(3);
-        std::cout<<"n0 id:"<<n0.getidNote()<<std::endl;
-        std::cout<<"n2 id:"<<n2.getidNote()<<std::endl;
+        std::cout<<"n0 id:"<<n0.getIdNote()<<std::endl;
+        std::cout<<"n2 id:"<<n2.getIdNote()<<std::endl;
         std::cout<<n2.getDateCrea().getJour()<<" "<<n2.getDateCrea().getMois()<<" "<<n2.getDateCrea().getAnnee()<<std::endl;
 
         /*for(std::vector<Note*>::iterator it=manager.listNotes.begin(); it!=manager.listNotes.end(); ++it)
-            std::cout<<(*it)->getidNote()<<std::endl;*/
+            std::cout<<(*it)->getIdNote()<<std::endl;*/
         Note& n4=manager.getNote(5);
         NoteVersion test;
         std::cout<<"fin du try plurinotes"<<std::endl;
+
+        Relation* rReference = new Relation("Reference", "Cette relation permet de matérialiser les références qu’une note peut faire à d’autres notes grâce à une syntaxe spéciale",true);
+
 
     }
     catch(Exception& e){std::cout<<e.getInfo().toStdString()<<std::endl;}
